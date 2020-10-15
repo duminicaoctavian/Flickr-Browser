e0d5009cc4f6aed5f5ecf7693728fe926b2c675e
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-15 19:13:05 +0300

    GestureDetector

--- app/src/main/java/com/octavian/flickrbrowser/RecyclerItemClickListener.kt
+++ app/src/main/java/com/octavian/flickrbrowser/RecyclerItemClickListener.kt
@@ -2,8 +2,10 @@ package com.octavian.flickrbrowser
 
 import android.content.Context
 import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "RecyclerItemClickListen"
@@ -16,8 +18,34 @@ class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, pr
         fun onItemLongClick(view: View, position: Int)
     }
 
+    // add the gesture detector -> anonymous class
+    private val gestureDetector = GestureDetectorCompat(context, object: GestureDetector.SimpleOnGestureListener() {
+
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            Log.d(TAG, ".onSingleTapUp: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, ".onSingleTapUp calling listener.onItemClick")
+            if (childView != null) {
+                listener.onItemClick(childView, recyclerView.getChildAdapterPosition(childView))
+            }
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            Log.d(TAG, ".onLongPress: starts")
+            val childView = recyclerView.findChildViewUnder(e.x, e.y)
+            Log.d(TAG, ".onLongPress calling listener.onItemLongClick")
+            if (childView != null) {
+                listener.onItemLongClick(childView, recyclerView.getChildAdapterPosition(childView))
+            }
+        }
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         Log.d(TAG, ".onInterceptTouchEvent: starts $e")
+        val result = gestureDetector.onTouchEvent(e)
+        Log.d(TAG, ".onInterceptTouchEvent() returning: $result")
+//        return super.onInterceptTouchEvent(rv, e)
         return true
     }
 }
