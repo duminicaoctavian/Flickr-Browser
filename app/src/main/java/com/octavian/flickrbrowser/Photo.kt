da764b2aadf55deaa3106d1d86e2117107fb6c3d
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-15 20:12:26 +0300

    Parcelable

--- app/src/main/java/com/octavian/flickrbrowser/Photo.kt
+++ app/src/main/java/com/octavian/flickrbrowser/Photo.kt
@@ -1,11 +1,7 @@
 package com.octavian.flickrbrowser
 
 import android.os.Parcelable
-import android.util.Log
 import kotlinx.android.parcel.Parcelize
-import java.io.IOException
-import java.io.ObjectStreamException
-import java.io.Serializable
 
 private const val TAG = "Photo"
 
@@ -18,38 +14,38 @@ class Photo(var title: String, var author: String, var authorId: String,
     // Serializable can be slow due to reflection
     // That is why we need to implement additional methods
     // serialVersionUID is here for compatibility purposes between JVMs
-    companion object {
-        private const val serialVersionUID = 1L
-    }
+//    companion object {
+//        private const val serialVersionUID = 1L
+//    }
 
     override fun toString(): String {
         return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tags='$tags', image='$image')"
     }
 
-    @Throws(IOException::class)
-    private fun writeObject(out: java.io.ObjectOutputStream) {
-        Log.d(TAG, "writeObject called")
-        out.writeUTF(title)
-        out.writeUTF(author)
-        out.writeUTF(authorId)
-        out.writeUTF(link)
-        out.writeUTF(tags)
-        out.writeUTF(image)
-    }
-
-    @Throws(IOException::class, ClassNotFoundException::class)
-    private fun readObject(inStream: java.io.ObjectInputStream) {
-        Log.d(TAG, "readObject called")
-        title = inStream.readUTF()
-        author = inStream.readUTF()
-        authorId = inStream.readUTF()
-        link = inStream.readUTF()
-        tags = inStream.readUTF()
-        image = inStream.readUTF()
-    }
-
-    @Throws(ObjectStreamException::class)
-    private fun readObjectNoData() {
-        Log.d(TAG, "readObjectNoData called")
-    }
+//    @Throws(IOException::class)
+//    private fun writeObject(out: java.io.ObjectOutputStream) {
+//        Log.d(TAG, "writeObject called")
+//        out.writeUTF(title)
+//        out.writeUTF(author)
+//        out.writeUTF(authorId)
+//        out.writeUTF(link)
+//        out.writeUTF(tags)
+//        out.writeUTF(image)
+//    }
+//
+//    @Throws(IOException::class, ClassNotFoundException::class)
+//    private fun readObject(inStream: java.io.ObjectInputStream) {
+//        Log.d(TAG, "readObject called")
+//        title = inStream.readUTF()
+//        author = inStream.readUTF()
+//        authorId = inStream.readUTF()
+//        link = inStream.readUTF()
+//        tags = inStream.readUTF()
+//        image = inStream.readUTF()
+//    }
+//
+//    @Throws(ObjectStreamException::class)
+//    private fun readObjectNoData() {
+//        Log.d(TAG, "readObjectNoData called")
+//    }
 }
