7724b264915b9fb14910523cf9cc235e94ea6453
Octavian Duminica <duminicaoctavian@gmail.com>
2020-10-15 19:54:24 +0300

    Serializable

--- app/src/main/java/com/octavian/flickrbrowser/Photo.kt
+++ app/src/main/java/com/octavian/flickrbrowser/Photo.kt
@@ -1,9 +1,50 @@
 package com.octavian.flickrbrowser
 
-class Photo(val title: String, val author: String, val authorId: String,
-            val link: String, val tags: String, val image: String) {
+import android.util.Log
+import java.io.IOException
+import java.io.ObjectStreamException
+import java.io.Serializable
+
+private const val TAG = "Photo"
+
+class Photo(var title: String, var author: String, var authorId: String,
+            var link: String, var tags: String, var image: String) : Serializable {
+
+    // Serializable can be slow due to reflection
+    // That is why we need to implement additional methods
+    // serialVersionUID is here for compatibility purposes between JVMs
+    companion object {
+        private const val serialVersionUID = 1L
+    }
 
     override fun toString(): String {
         return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tags='$tags', image='$image')"
+    }
+
+    @Throws(IOException::class)
+    private fun writeObject(out: java.io.ObjectOutputStream) {
+        Log.d(TAG, "writeObject called")
+        out.writeUTF(title)
+        out.writeUTF(author)
+        out.writeUTF(authorId)
+        out.writeUTF(link)
+        out.writeUTF(tags)
+        out.writeUTF(image)
+    }
+
+    @Throws(IOException::class, ClassNotFoundException::class)
+    private fun readObject(inStream: java.io.ObjectInputStream) {
+        Log.d(TAG, "readObject called")
+        title = inStream.readUTF()
+        author = inStream.readUTF()
+        authorId = inStream.readUTF()
+        link = inStream.readUTF()
+        tags = inStream.readUTF()
+        image = inStream.readUTF()
+    }
+
+    @Throws(ObjectStreamException::class)
+    private fun readObjectNoData() {
+        Log.d(TAG, "readObjectNoData called")
     }
 }
